b37e1ffee86144b0915261ac155f20e717a86ecb
chheangg <balldremz@gmail.com>
2021-12-12 13:16:09 +0700

    Add UI and Functionality

---
+++ script.js
@@ -0,0 +1,66 @@
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+let input;
+button.addEventListener('click', () => {
+    for ( i = calculateGrid(input); i > 0; i-- ) {
+        container.removeChild(container.lastElementChild);
+    }
+    playRound();
+    }
+);
+
+playRound();
+
+function playRound() {
+prompting();
+createContainer(input);
+createGrid(calculateGrid(input));
+gridTransition();
+}
+
+
+function prompting() {
+    input = prompt("How much lah?");  
+
+    if (input === undefined || input === null || input > 100) {
+        return prompting();
+    }
+
+    input;
+}
+function calculateGrid(input) {
+    parseInt(input);
+    let grid = input ** 2;
+    return grid;
+}
+
+function createContainer(input) {
+    container.style.cssText = `grid-template-columns: repeat(${input}, auto);`;
+}
+
+function createGrid(input) {
+    for (i = 0; i < input; i++) {
+        createOneGrid();
+    }
+}
+
+function createOneGrid() {
+    let grid = document.createElement('div');
+    grid.classList.add('grid'); 
+    container.appendChild(grid);
+}
+
+function gridTransition() {
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(gridHover);
+}
+
+function gridHover(gridName) {
+    gridName.addEventListener('mouseenter', (gridEvent) => {
+        gridEvent.target.style.cssText = `background-color: rgb(${randomRGB()},${randomRGB()},${randomRGB()})`;
+
+        setTimeout((e) => {
+            gridEvent.target.style.cssText = 'background-color: white';
+        }, 500)
+    } )
+}
